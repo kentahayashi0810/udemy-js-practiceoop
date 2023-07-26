b2e679cee6ab786f7bc89bb481af39b70ff2c973
kentahayashi <kenta0810hayashi@gmail.com>
2023-07-26 08:38:15 +0200

    278~282. Passing Method References Around

---
+++ assets/scripts/app.js
@@ -0,0 +1,48 @@
+class Tooltip {}
+
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchButton = projectItemElement.querySelector(
+      "button:last-of-type"
+    );
+    switchButton.addEventListener("click");
+  }
+}
+
+class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProjects() {}
+
+  switchProjects(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+  }
+}
+
+App.init();
